23cbdb1eac2b9b4ab727265b500b00e35ebad057
littlemonster00 <sang.lequang94@gmail.com>
2019-12-21 23:13:35 +0700

    Add open new file overload file that previous

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { ipcRenderer, remote } = require("electron");
 const editor = document.getElementById("editor");
 ipcRenderer.on("new-file", (event, args) => {
+  const textArea = document.getElementById("text-editor");
+  if (textArea) {
+    if (document.title.split(" ")[0] === "*") {
+      alert("Please save this file before open new windows");
+      return;
+    }
+    textArea.parentNode.removeChild(textArea);
+  }
   if (args) {
     const div = document.createElement("div");
     div.id = "text-editor";
     div.innerHTML = `
-    <textarea name="" id="textArea" cols="125" rows="40"></textarea>
+    <textarea name="" id="textArea" cols="120" rows="40"></textarea>
   `;
     document.getElementById("editor").appendChild(div);
     document.title = "Untitled";
@@ -14,7 +22,7 @@ ipcRenderer.on("new-file", (event, args) => {
 });
 function addSaveStateListener() {
   const textArea = document.getElementById("textArea");
-  textArea.addEventListener("keypress", (event, logKey) => {
+  textArea.addEventListener("keydown", logKey => {
     const oldTitle = document.title;
     if (oldTitle.split(" ")[0] !== "*") {
       document.title = "* " + oldTitle;
@@ -38,10 +46,18 @@ ipcRenderer.on("save-file", (event, args) => {
 
 ipcRenderer.on("file-open", (event, args) => {
   if (args) {
+    const textArea = document.getElementById("text-editor");
+    if (textArea) {
+      // if (document.title.split(" ")[0] === "*") {
+      //   alert("Please save this file before open new windows");
+      //   return;
+      // }
+      textArea.parentNode.removeChild(textArea);
+    }
     document.title = args.filePath.split("/").pop();
     const div = document.createElement("div");
     div.id = "text-editor";
-    div.innerHTML = `<textarea name=${args.filePath} id="textArea" cols="125" rows="40">${args.text}</textarea>`;
+    div.innerHTML = `<textarea name=${args.filePath} id="textArea" cols="120" rows="40">${args.text}</textarea>`;
     document.getElementById("editor").appendChild(div);
     addSaveStateListener();
   }
